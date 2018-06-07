aeab1b3a58ed09b1b7069516ad2e0dda84a61f19
Stacey Tyler <sltyler15@gmail.com>
2018-06-06 19:24:12 -0500

    CG1_COMPLETE

---
+++ CG1/main.js
@@ -0,0 +1,16 @@
+console.log("This file is linked!");
+var name = "Yoda";
+var department = "Jedi";
+var TeacherRating1 = 3.4
+var TeacherRating2 = 5.0
+var TeacherRating3 = 4.2
+var avgRating = ((TeacherRating1+TeacherRating2+TeacherRating3)/3)
+var students = "Yolo"
+var courses = "Course1"
+
+console.log ("Teacher: "+name)
+console.log ("Department: "+department)
+console.log ("Ratings: "+TeacherRating1+', '+TeacherRating2.toFixed(1)+', '+TeacherRating3)
+console.log ("Average: "+avgRating.toFixed(1))
+console.log ("Student: "+students)
+console.log ("Course: "+courses)
