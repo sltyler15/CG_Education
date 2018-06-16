5153d81a8ec14cde30dd205419d02c01ffb957b2
Stacey Tyler <sltyler15@gmail.com>
2018-06-16 12:04:55 -0500

    CG2_Complete

--- CG1/main.js
+++ CG1/main.js
@@ -37,17 +37,46 @@ function addTeacherRating(teacherarray, newRating)
 addTeacherRating(teacherRatings, 4)
 console.log(teacherRatings)
 
-/* CG Education: Part 2: Step 4 */
-var teacherPrompt = prompt("We would like for you to review.  Please enter a rating between 0.0 - 5.0?");
+/* CG Education: Challenge 1: Step 2 */
+/* var teacherPrompt = prompt("We would like for you to review.  Please enter a rating between 0.0 - 5.0?");
 
 if (teacherPrompt <0.0 || teacherPrompt >5.0) {
   alert("Try Again!");
 }
 
 addTeacherRating(teacherRatings, teacherPrompt)
-console.log(teacherRatings)
+console.log(teacherRatings) */
 
 
 var finalRating=getRatingAvg(teacherRatings)
 console.log(finalRating)
 alert("Thanks for your review! "+name+" average rating is now "+finalRating)
+
+var courses =
+[
+  ["Algebra1","Mathematics1"],
+  ["Geometry","Mathematics2"],
+  ["College Algebra","Mathematics4"],
+  ["Calculus","Mathematics4"],
+  ["Algebra2","Mathematics3"]
+]
+/* CG Education: Challenge 2: Step 3 */
+function courseFilter(array, department)
+{return array.filter(course =>course[1]==department);
+
+}
+
+var secondPrompt = prompt("What department is the course in?");
+
+var finallyFiltered=courseFilter(courses,secondPrompt);
+if (finallyFiltered.length == 0){
+  alert("Try Again!")
+}
+else {
+  var courselist = "";
+  for(var i = 0; i < finallyFiltered.length; i++) {
+      courselist += finallyFiltered[i][0]+ "\n";
+    }
+  alert("Please select a course from the list below: "+"\n"+"\n" +courselist)
+console.log(courselist)
+}
